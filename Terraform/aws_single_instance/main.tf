2456bd16a17c625ef5fe05abdbfe8d67e027bd54
prabhu9683 <54091488+prabhu9683@users.noreply.github.com>
2021-03-27 06:38:00 +0530

    Update main.tf

--- Terraform/aws_single_instance/main.tf
+++ Terraform/aws_single_instance/main.tf
@@ -7,6 +7,6 @@ provider "aws" {
      region = "ap-southeast-1"
  }
  resource "aws_instance" "TEST-EC2" {
-     ami = "ami-0cd0a56020efb4c09"
+     ami = "ami-0ba0ce0c11eb723a1"
      instance_type = "t2.micro"
 }
