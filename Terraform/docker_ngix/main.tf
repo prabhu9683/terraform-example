7adfa57a45cf6108e7136410cf3f2e53debdd092
prabhu9683 <54091488+prabhu9683@users.noreply.github.com>
2021-03-28 23:16:55 +0530

    Update main.tf

--- Terraform/docker_ngix/main.tf
+++ Terraform/docker_ngix/main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = "$ {docker_image.nginx.latest}"
   name  = "nginx_server"
   ports {
     internal = 80
